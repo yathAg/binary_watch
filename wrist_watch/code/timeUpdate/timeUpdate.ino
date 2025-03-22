b65a72b0b42ea1792e054ba9dc86d193bfe69468
yathAg <yatharthrpi@gmail.com>
2025-03-22 01:36:38 -0400

    New code ( not tested)

--- wrist_watch/code/timeUpdate/timeUpdate.ino
+++ wrist_watch/code/timeUpdate/timeUpdate.ino
@@ -1,153 +1,191 @@
-// Low Power settings
-// BOD 1.8v
-// BootLoader Yes (UART0)
-// CLock Internal 1Mhz
-// EEPROM EEPROM retained
-// Compiler LTO LTO Disabled
-// Varient 328/328PA
+#include <Wire.h>
+#include "RTClib.h"
+#include "LowPower.h"
 
 //-----------LED Settings-----------
-const int row[4] = {3, 4, 5, 6};  // pins for A, B, C, D rows in the LED matrix. see drawing in instructions
-const int col[4] = {7, 8, 9, 10}; // pins for 1, 2, 3, 4 columns in the LED matrix
-byte rowByte[4] = {0, 0, 0, 0};   // table that stores and indexes the bytes used to display
-int rowCount = 0;                 // index used while looping through bytes to display on matrix
-int delayTime = 300;              // microseconds. delay time between each row beeing displayed in LED matrix. scanning display
+const int row[4] = {3, 4, 5, 6};       // LED matrix row pins
+const int col[4] = {7, 8, 9, 10};      // LED matrix column pins
+byte rowByte[4] = {0, 0, 0, 0};        // Stores the digits to display
 
-//-----------Sleep Settings-----------
-#include "LowPower.h"             //library for deep sleep
-unsigned long onTime = 10 * 1000; // time display stays on before going to deep sleep
-unsigned long sleepTimer = 0;     // used to time how long the watch has been awake
-volatile bool woke = false;       // AF
-int pushBtn = 2;                  // pin for push button used to interrupt sleep
-bool awakened = false;
+//-----------RTC & Time Settings-----------
+RTC_DS3231 rtc;
 
-//-----------Time Settings-----------
-#include <Wire.h> //i2c library
-#include "RTClib.h"
-RTC_DS3231 rtc;         // name of attached RTC
-int timeOrDate = false; // indicates which display option to show. time or date
-DateTime now;
+//-----------Modes & Sleep Settings-----------
+enum Mode { NORMAL, EDIT_HOUR, EDIT_MINUTE };
+Mode currentMode = NORMAL;
+bool displayTimeMode = true;           // true: show time; false: show date
+unsigned long onTime = 10 * 1000;        // Display active time before sleep
+unsigned long sleepTimer = 0;
 
-//-----------Other Variables-----------
-volatile unsigned long prevInterrupt = 0; // used for debouncing push button
-void wakeFunction();
-void displayRow(int, byte);
+//-----------Button Debounce & Press Handling-----------
+const int pushBtn = 2;
+const unsigned long debounceDelay = 50;
+const unsigned long longPressThreshold = 1000; // 1 second
+bool buttonState = HIGH;              // current reading from button (pull-up so HIGH means unpressed)
+bool lastButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+unsigned long pressStartTime = 0;
+bool buttonHandled = false;
 
-void setup()
-{
+//-----------LED Multiplexing (Non-blocking)-----------
+unsigned long lastDisplayUpdate = 0;
+const unsigned long displayInterval = 2; // ms per row update
+int currentRowDisplay = 0;
 
+//-----------Edit Mode Variables-----------
+int newHour = 0, newMinute = 0;
+
+void setup() {
   rtc.begin();
-  // do you want to set the time? uncomment this line after first upload and time has been set
-  //rtc.adjust(DateTime(2023, 04, 12, 00, 16, 0)); // year, month, day, hour, minute, second
-
+  // Uncomment and adjust to set the RTC initially:
+  // rtc.adjust(DateTime(2023, 04, 12, 0, 16, 0));
+  
   pinMode(pushBtn, INPUT_PULLUP);
-  for (int i = 0; i < 4; i++) // Defines all LEDs pins as OUTPUT
-  {
+  for (int i = 0; i < 4; i++) {
     pinMode(row[i], OUTPUT);
     pinMode(col[i], OUTPUT);
   }
-
   allOff();
-  delay(40);
-  attachInterrupt(0, wakeFunction, FALLING);
+  sleepTimer = millis();
 }
 
-void wakeFunction() // interrupt function when button is pressed
-{
-  woke = true;              // flags the microcontroller as just woken up
-  prevInterrupt = millis(); // saves millis for debouncing
-  // goes to main loop
-}
+void loop() {
+  // Refresh LED display using non-blocking multiplexing
+  updateDisplay();
 
-void displaytime() // handles one row to display on the matrix at the time
-{
-  for (int i = 0; i < 4; i++)
-  {
-    for (int j = 0; j < 4; j++)
-    {
-      int k;
-      k = bitRead(rowByte[i], j);
-
-      if (k == 1)
-      {
-        digitalWrite(row[i], LOW);
-        digitalWrite(col[j], HIGH);
-        delay(1);
+  // --- Button Handling (Debounce & Press Duration) ---
+  int reading = digitalRead(pushBtn);
+  unsigned long currentTime = millis();
+  
+  if (reading != lastButtonState) {
+    lastDebounceTime = currentTime;  // reset the debounce timer
+  }
+  if ((currentTime - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == LOW) {
+        // Button pressed: record time and mark as not handled
+        pressStartTime = currentTime;
+        buttonHandled = false;
+      } else {
+        // Button released: if not already handled, determine press type
+        if (!buttonHandled) {
+          unsigned long pressDuration = currentTime - pressStartTime;
+          handleButtonPress(pressDuration);
+          buttonHandled = true;
+        }
       }
-      else if (k = 0)
-      {
-        digitalWrite(row[i], HIGH);
-        digitalWrite(col[j], LOW);
-        delay(1);
-      }
-      digitalWrite(row[i], HIGH);
-      digitalWrite(col[j], LOW);
-      delay(0.5);
     }
   }
-}
-
-void allOff() // turn all IO off before going to sleep to save power
-{
-  for (int i = 0; i < 4; i++)
-  {
-    digitalWrite(row[i], LOW); // rows must be set low to turn off
-    digitalWrite(col[i], LOW); // columns must be set high to turn off
+  lastButtonState = reading;
+  
+  // Reset sleep timer on any button activity
+  if (buttonState == LOW || currentMode != NORMAL) {
+    sleepTimer = currentTime;
   }
-}
-
-void loop()
-{
-  if (woke)
-  {
-    if (millis() - prevInterrupt > 100) // if button has been pressed for 100ms
-    {
-      if (!digitalRead(pushBtn))
-      {
-        awakened = true;          // flag to start led matrix
-        timeOrDate = !timeOrDate; // toggle between time and day being shown
-        sleepTimer = millis();    // start the sleep countdown
-        woke = false;
-        now = rtc.now(); // gets the current time from DST
-      }
-      else
-        woke = false;
-    }
-  }
-
-  if (awakened)
-  {
-    if (millis() - sleepTimer < onTime) // if display has been on for less than 30 sec
-    {
-      if (timeOrDate) // loads the current time into bytes to be displayed
-      {
-        rowByte[0] = now.hour() / 10;   // hour second digit
-        rowByte[1] = now.hour() % 10;   // hour first digit
-        rowByte[2] = now.minute() / 10; // minute second digit
-        rowByte[3] = now.minute() % 10; // minute first digit
-      }
-      else // loads the day and month into bytes to be displayed
-      {
-        rowByte[0] = now.day() / 10;   // day first digit
-        rowByte[1] = now.day() % 10;   // day second digit
-        rowByte[2] = now.month() / 10; // month first digit
-        rowByte[3] = now.month() % 10; // month second digit
-      }
-
-      displaytime(); // shows one byte at the time on the matrix
-    }
-    else // after display complete
-    {
-      timeOrDate = false; // set to false so time is first thing being shown on wakeup
-      woke = false;
-      awakened = false;
-    }
-  }
-
-  if (!woke && !awakened)
-  {
+  
+  // Enter low-power sleep if in NORMAL mode and inactive
+  if (currentMode == NORMAL && (currentTime - sleepTimer > onTime)) {
     allOff();
-    delay(30);
-    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); // zzzz
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+  }
+}
+
+// Called when a button press is completed
+void handleButtonPress(unsigned long duration) {
+  if (currentMode == NORMAL) {
+    if (duration >= longPressThreshold) {
+      // Long press: Enter hour edit mode and initialize new values from RTC
+      DateTime now = rtc.now();
+      newHour = now.hour();
+      newMinute = now.minute();
+      currentMode = EDIT_HOUR;
+    } else {
+      // Short press: Toggle between time and date display
+      displayTimeMode = !displayTimeMode;
+    }
+  }
+  else if (currentMode == EDIT_HOUR) {
+    if (duration >= longPressThreshold) {
+      // Long press: Confirm hour and move to minute editing
+      currentMode = EDIT_MINUTE;
+    } else {
+      // Short press: Increment hour (cycles 0-23)
+      newHour = (newHour + 1) % 24;
+    }
+  }
+  else if (currentMode == EDIT_MINUTE) {
+    if (duration >= longPressThreshold) {
+      // Long press: Confirm minute, update RTC, and return to normal mode
+      DateTime now = rtc.now();
+      DateTime newTime(now.year(), now.month(), now.day(), newHour, newMinute, now.second());
+      rtc.adjust(newTime);
+      currentMode = NORMAL;
+    } else {
+      // Short press: Increment minute (cycles 0-59)
+      newMinute = (newMinute + 1) % 60;
+    }
+  }
+}
+
+// Non-blocking LED matrix update function
+void updateDisplay() {
+  // Update the display content based on mode
+  if (currentMode == NORMAL) {
+    DateTime now = rtc.now();
+    if (displayTimeMode) {
+      rowByte[0] = now.hour() / 10;
+      rowByte[1] = now.hour() % 10;
+      rowByte[2] = now.minute() / 10;
+      rowByte[3] = now.minute() % 10;
+    } else {
+      rowByte[0] = now.day() / 10;
+      rowByte[1] = now.day() % 10;
+      rowByte[2] = now.month() / 10;
+      rowByte[3] = now.month() % 10;
+    }
+  }
+  else if (currentMode == EDIT_HOUR) {
+    rowByte[0] = newHour / 10;
+    rowByte[1] = newHour % 10;
+    rowByte[2] = 0;
+    rowByte[3] = 0;
+  }
+  else if (currentMode == EDIT_MINUTE) {
+    rowByte[0] = newMinute / 10;
+    rowByte[1] = newMinute % 10;
+    rowByte[2] = 0;
+    rowByte[3] = 0;
+  }
+  
+  // Non-blocking multiplexing: update one row every displayInterval ms
+  unsigned long currentTime = millis();
+  if (currentTime - lastDisplayUpdate >= displayInterval) {
+    // First, turn off all rows and columns
+    for (int i = 0; i < 4; i++) {
+      digitalWrite(row[i], HIGH);
+      digitalWrite(col[i], LOW);
+    }
+    // For the current row, set columns based on the bit pattern in rowByte
+    for (int j = 0; j < 4; j++) {
+      int bitVal = bitRead(rowByte[currentRowDisplay], j);
+      if (bitVal == 1) {
+        digitalWrite(row[currentRowDisplay], LOW);
+        digitalWrite(col[j], HIGH);
+      } else {
+        digitalWrite(row[currentRowDisplay], HIGH);
+        digitalWrite(col[j], LOW);
+      }
+    }
+    currentRowDisplay = (currentRowDisplay + 1) % 4;
+    lastDisplayUpdate = currentTime;
+  }
+}
+
+// Turn off all LED outputs (useful before sleep)
+void allOff() {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(row[i], LOW);
+    digitalWrite(col[i], LOW);
   }
 }
